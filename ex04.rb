0d14a2466eb823f4804046fd69b6ccdc8018c6da
paskuvan <majopaskuvan@gmail.com>
2019-01-08 23:00:47 -0300

    four commit

--- ex04.rb
+++ ex04.rb
@@ -1,5 +1,6 @@
-class Semana
+module Semana
   @@primer_dia = 'Lunes'
+
   def self.primer_dia
     @@primer_dia
   end
@@ -7,10 +8,12 @@ class Semana
   def self.en_un_meses
     "Un mes tiene 4 semanas."
   end
+
   def self.en_un_año
     "Un año tiene 52 semanas."
   end
 end
+
 puts "La semana comienza el día #{Semana.primer_dia}"
 puts Semana.en_un_meses
 puts Semana.en_un_año
